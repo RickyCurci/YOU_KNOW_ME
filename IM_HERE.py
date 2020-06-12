f6bcb84f61977a91dbaaf1aa2d2ff4240d185778
RickyCurci <ric.cur@gmail.com>
2020-06-12 12:05:48 +0200

    README

--- IM_HERE.py
+++ IM_HERE.py
@@ -1,1 +1,72 @@
 #YOU_KNOW_ME
+QUESTION_1 = "What is my favourite car (Model) ?"
+QUESTION_2 = "My politic orientation ?"
+QUESTION_3 = "My big difect ?"
+
+print(QUESTION_1)
+ANSWER_C1 = "Valkyre"
+ANSWER_1 = input(">:")
+
+if ANSWER_1 == ANSWER_C1: 
+  print("-------------------------")
+  print("{CORRECT} [1/3]")
+  
+  print("\n"*100)
+  
+  print(QUESTION_2)
+  ANSWER_C2 = "Liberalist"
+  ANSWER_2 = input(">:")
+  
+  if ANSWER_2 == ANSWER_C2: 
+    print("-------------------------")
+    print("{CORRECT} [2/3]")
+    
+    print("\n"*100)
+    
+    print(QUESTION_3)
+    ANSWER_C3 = "ANXIETY"
+    ANSWER_3 = input(">:")
+    
+    if ANSWER_3 == ANSWER_C3:
+      print("-------------------------")
+  	  print("{CORRECT} [3/3]")
+      
+      print("\n"*100)
+      
+      print("GOOD JOB!!")
+      print("=========================")
+      print("NOW OPEN YOU BROWSER AND ENTER: http://www.encryptdecrypt.net/decriptare.php")
+      print("TEXT: 0A077E681133C35E5428152F01755C2B54")
+      print("KEY: 2 4 0 9 1 6")
+      
+      
+      PASSWORD_C ="SONOSORPRESO402"
+      PASSWORD = input("DECRYPTED TEXT >: ")
+      
+      if PASSWORD == PASSWORD_C: 
+        print("\n"*100)
+        print("OK... NOW OPEN NEW TAB IN YOUR BROWSER AND ENTER THIS LINK: https://github.com/login")
+        print("USERNAME: SEIUNODEIPOCHI")
+        print("PASSWORD: SONOSORPRESO402")
+        
+      else {
+        
+        print("TEXT INCCORECT")
+        
+      }
+    
+    else {
+      print("-------------------------")
+  	  print("{INCORRECT} [2/3]")
+    }
+  else {
+    print("-------------------------")
+  	print("{INCORRECT} [1/3]")
+  
+  }
+    
+else {
+  print("-------------------------")
+  print("{INCORRECT} [0/3]")
+  
+}
